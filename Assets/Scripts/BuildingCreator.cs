4cfd78ef15bd3d87eafae6c13e2c01d4d380d4a4
Felipe Pedroso <f.pedroso677@gmail.com>
2015-08-25 22:29:38 -0700

    Improving procedural scenario generation.
    Signed-off-by: Felipe Pedroso <f.pedroso677@gmail.com>

---
+++ Assets/Scripts/BuildingCreator.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildingCreator : MonoBehaviour {
+    public GameObject FloorGameObject;
+
+    private int count = 0;
+
+    void Start()
+    {
+        StartCoroutine("CreateBuildings");
+
+    }
+
+    IEnumerator CreateBuildings()
+    {
+        Debug.Log("Creating a MF buldings for the MF " + transform.parent.gameObject.name);
+        Vector3 floorLocalScale = FloorGameObject.transform.localScale;
+
+
+        for (int i = 0; i < 2; i++)
+        {
+            int side = (i % 2 == 0 ? 1 : -1);
+
+            float nextZ = 0;
+
+            while (nextZ < floorLocalScale.z * 10)
+            {
+                GameObject randomBuilding = GenerateRandomBuilding();
+                Vector3 buildingScale = new Vector3(Random.Range(1, 3), Random.Range(5, 10), 1);
+                randomBuilding.transform.localScale = buildingScale;
+
+                float xPosition = ((floorLocalScale.x * 5) + (buildingScale.x * 0.5f)) * side;
+                float yPosition = buildingScale.y * 0.5f;
+                float zPosition = nextZ + transform.parent.transform.position.z - floorLocalScale.z * 5f +(buildingScale.z * 0.5f);
+
+                Vector3 buildingPosition = new Vector3(xPosition, yPosition, zPosition);
+                randomBuilding.transform.position = buildingPosition;
+
+                nextZ += (buildingScale.z);
+            }
+                
+
+                yield return null;
+        }
+        
+    }
+
+    private GameObject GenerateRandomBuilding() 
+    {
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+        cube.transform.parent = gameObject.transform;
+
+        cube.GetComponent<MeshRenderer>().material.color = new Color(Random.value, Random.value, Random.value);
+
+        return cube;
+    }
+}
--- Assets/Scripts/PlatformCreator.cs
+++ Assets/Scripts/PlatformCreator.cs
@@ -3,7 +3,13 @@ using System.Collections;
 
 public class PlatformCreator : MonoBehaviour {
     public GameObject FloorPrefab;
-    public Transform AttachPoint;
+    public Transform CurrentPlaneTransform;
+
+    public static int PlataformNameCount = 1;
+    void Start() 
+    {
+        FloorPrefab = Resources.Load<GameObject>("Prefabs/Floor");
+    }
 
     void OnTriggerEnter(Collider other) 
     {
@@ -11,11 +17,14 @@ public class PlatformCreator : MonoBehaviour {
         {
             if (FloorPrefab != null)
             {
-                Vector3 position = AttachPoint.position + Vector3.forward * FloorPrefab.transform.GetChild(0).transform.localScale.z * 0.5f;
+                float creatorZ = gameObject.transform.position.z;
+                //Vector3 position = AttachPoint.position + Vector3.forward * FloorPrefab.transform.GetChild(0).transform.localScale.z * 0.5f;
+                Vector3 position = new Vector3(0, 0, CurrentPlaneTransform.position.z + (CurrentPlaneTransform.localScale.z * 10));
                 GameObject newFloor = (GameObject)Instantiate(FloorPrefab, position, Quaternion.identity);
-                newFloor.name = "Floor";
+                newFloor.name = "Floor" + PlataformNameCount++;
 
             }
         }
+        Destroy(gameObject);
     }
 }

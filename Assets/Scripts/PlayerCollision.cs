458c48423dfcc88bedf817f00fc002817933db61
Felipe Pedroso <f.pedroso677@gmail.com>
2015-10-09 16:50:56 -0300

    Refininf player movement, refactoring some classes, delegating proper responsabilities
    Signed-off-by: Felipe Pedroso <f.pedroso677@gmail.com>

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCollision : MonoBehaviour {
+    public float Strenght;
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody body = hit.collider.attachedRigidbody;
+        if (body == null || body.isKinematic)
+            return;
+
+        if (hit.moveDirection.y < -0.3F)
+            return;
+
+        Vector3 pushDir = new Vector3(hit.moveDirection.x, hit.moveDirection.y, hit.moveDirection.z);
+        body.velocity = pushDir * Strenght;
+
+        if (hit.gameObject.tag.ToLower().Equals("obstacle"))
+        {
+            ObstacleBehaviour ob = hit.gameObject.GetComponent<ObstacleBehaviour>();
+
+            if (ob != null)
+            {
+                gameObject.SendMessage("TakeDamage", ob.DamageAmount);
+            }
+        }
+    }
+}
